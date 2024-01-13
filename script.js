8fbb576062a6d54e154f954800c9703091388175
g.skrylnikov <grisha2182@gmail.com>
2024-01-13 03:37:25 +0300

    add txt file

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 $(function () {
+
+    let word = '';
+
+    $.ajax({
+        url: 'test.txt',
+        dataType: 'text',
+        success: function (data) {
+            ChoiseWord(data);
+            useWordInFurtherCode();
+        }
+    });
+    
+    function ChoiseWord(data) {
+        let arrayOfLines = data.split(' ');
+        arrayOfLines = arrayOfLines.filter(function(line) {
+            return line.trim() !== '';
+        });
+        if (arrayOfLines.length > 0) {
+            let randomWord = arrayOfLines[Math.floor(Math.random() * arrayOfLines.length)];
+            word = randomWord;
+        }
+    }
+    
+    function useWordInFurtherCode() {
+        console.log("Selected word:", word);
+    }
+    
     let count = 0;
     let attempt = 0;
     $(".js-wrapper-key").on("click", function () {
-        console.log("click");
         let key = $(this).html();
         $(".js-input-key").each(function () {
             if ($(this).html() === ''){
@@ -16,14 +42,27 @@ $(function () {
         }
     });
 
-    $(".js-wrapper-box-accept").on("click", function () {
-        console.log("len = ", $(".wrapper__input").length);
-        // for (let i = 0; i < $(".wrapper__input").length; i += 1){
+    $(".wrapper__box_input").on("click", function () {
+        if ($(this).length > 0 && $(this).hasClass('disable') === false) {
+            $(this).html('');
+        }
+    });
 
-        // }
-        $(".wrapper__input").css("top", "30px");
+    $(".js-wrapper-box-accept").on("click", function () {
         count = 0;
         attempt += 1;
-        $(this).addClass("hide");
-    })
+        $(".wrapper__box_input").each(function () {
+            $(this).addClass("disable");
+        });
+        $(".wrapper__container").prepend(`
+        <div class="wrapper__input">
+            <div class="wrapper__box wrapper__box_input"><p class="js-input-key wrapper__key"></p></div>
+            <div class="wrapper__box wrapper__box_input"><p class="js-input-key wrapper__key"></p></div>
+            <div class="wrapper__box wrapper__box_input"><p class="js-input-key wrapper__key"></p></div>
+            <div class="wrapper__box wrapper__box_input"><p class="js-input-key wrapper__key"></p></div>
+            <div class="wrapper__box wrapper__box_input"><p class="js-input-key wrapper__key"></p></div>
+        </div>
+        `);
+        $(this).addClass('hide');
+    });
 });
